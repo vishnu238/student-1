91bf174f15e4a6b742d5c5a709d3d9530d845c73
Shanjanars18 <shanjanars.20cse@kongu.edu>
2021-10-23 16:55:18 +0530

    Placement commit

---
+++ Placedetial.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+public class Placedetial {
+    
+
+
+      public static void main(String args[])
+      {
+           
+          Scanner SC=new Scanner(System.in);
+           
+          System.out.print(" Name: ");
+           String name=SC.nextLine();
+           System.out.print(" RollNumber: " );
+           int RollNumber=SC.nextInt();
+           System.out.print(" MobileNumber: " );
+           int MobileNumber=SC.nextInt();
+           System.out.print("  PersonalEmailid: ");
+           String PersonalEmailid=SC.nextLine();
+           System.out.print("   EduEmailid: ");
+           String EduEmailid=SC.nextLine();
+           System.out.print(" Dayschlor/hosteller: ");
+           int Dayschlororhosteller=SC.nextInt();
+           System.out.print("  Address: ");
+           String Address=SC.nextLine();
+           System.out.print(" gpa 1 sem: ");
+           int gpa1sem =SC.nextInt();
+           System.out.print(" gpa 2 sem: ");
+           int gpa2sem =SC.nextInt();
+           System.out.print(" gpa 3 sem: ");
+           int gpa3sem=SC.nextInt();
+           System.out.print(" cgpa 1 sem: ");
+           int cgpa1sem =SC.nextInt();
+           
+
+           
+
+
+         
+      }
+          
+}
+
