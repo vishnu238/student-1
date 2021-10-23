110ec41a4c24f55c0e9785aa2578844900ee3fc0
Shanjanars18 <shanjanars.20cse@kongu.edu>
2021-10-23 11:36:35 +0530

    first commit

---
+++ details.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class details {
+      public static void main(String args[])
+      {
+           
+          Scanner SC=new Scanner(System.in);
+           
+          System.out.print(" Name: ");
+           String name=SC.nextLine();
+           System.out.print("  Father Name: ");
+           String FatherName=SC.nextLine();
+           System.out.print("  MotherName: ");
+           String MotherName=SC.nextLine();
+           System.out.print(" MobileNumber: " );
+           int MobileNumber=SC.nextInt();
+           System.out.print(" Age: ");
+           int Age=SC.nextInt();
+           System.out.print("  Email: ");
+           String Email=SC.nextLine();
+           System.out.print(" Bloodgroup: ");
+           int Bloodgroup=SC.nextInt();
+
+
+
+         
+      }
+          
+}
+
+    
+
