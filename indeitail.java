ad9aab18f0fc0adda3c1273b8325f23ad44b6f5f
Shanjanars18 <shanjanars.20cse@kongu.edu>
2021-10-23 12:06:21 +0530

    first commit

---
+++ indeitail.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class indeitail {
+
+      public static void main(String args[])
+      {
+           
+          Scanner SC=new Scanner(System.in);
+           
+          System.out.print("  StudentName: ");
+           String StudentName=SC.nextLine();
+           System.out.print("  NomineeName: ");
+           String NomineeName=SC.nextLine();
+           System.out.print("  Releation of Nominee: ");
+           String ReleationofNominee=SC.nextLine();
+           System.out.print("  NomineeMobileNumber: " );
+           int  NomineeMobileNumber=SC.nextInt();
+           System.out.print("  NomineeAge: ");
+           int  NomineeAge=SC.nextInt();
+           System.out.print("  Email: ");
+           String Email=SC.nextLine();
+           System.out.print("  Occuption: ");
+           String  Occuption=SC.nextLine();
+           System.out.print(" NomineeAadhar Number: ");
+           int NomineeAadharNumber=SC.nextInt();
+
+
+
+         
+      }
+          
+}
+    
